a43d9efb6b3b3a0a2e6c39567bae7459f93fac39
Alexey Zauzin <lehadnk@gmail.com>
2018-08-10 11:38:40 +0300

    Added some debug info

--- src/MessageHandlers/DefaultMessageHandler.js
+++ src/MessageHandlers/DefaultMessageHandler.js
@@ -16,28 +16,36 @@ class DefaultMessageHandler {
 
     handle(msg)
     {
+        console.log('handle1');
         if (msg.author.bot) return;
+        console.log('handle2');
         if (this.client.user.id === msg.author.id) return;
+        console.log('handle3');
         if (this.syncChannels.indexOf(msg.channel.name) === -1) return;
+        console.log('handle4');
 
         // Is user in the ban list?
         if (this.bansRepository.getBannedDiscordUserIds().indexOf(msg.author.id) !== -1) return;
+        console.log('handle5');
 
-        // Is this user a newcomer?
+        Is this user a newcomer?
         if (this.isNewcomer(msg.author) && msg.channel.name !== 'crosschat-moder') {
             msg.author.sendMessage("К сожалению, мы были вынуждены включить защиту от спама в кросс-каналах. Поскольку вы недавно пришли на сервер, вам надо подождать немного времени прежде чем у вас появится возможность писать.");
             this.msgDeleteLogger.log(msg, "User is a newcomer");
             msg.delete().catch(() => console.log("Missing message management permissions in " + msg.guild.name));
             return;
         }
+        console.log('handle6');
 
         if (!this.floodProtector.canWrite(msg.author.id) && msg.channel.name !== 'crosschat-moder') {
             this.msgDeleteLogger.log(msg, "Flood protection");
             msg.delete().catch(() => console.log("Missing message management permissions in " + msg.guild.name));
             return;
         }
+        console.log('handle7');
 
         this.syncMessage(msg);
+        console.log('handle8');
         this.floodProtector.countMessage(msg);
     }
 
